5a2278647f4795d64b3feddafa4795b882f198ea
Zane <98283161+mrazekz1@users.noreply.github.com>
2022-04-18 17:06:41 -0400

    Update app.py
    Changed query that user enters token for

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def form():
 # handle venue POST and serve result web page
 @app.route('/venue-handler', methods=['POST'])
 def venue_handler():
-    rows = connect('SELECT venue_id, title FROM events WHERE venue_id = ' + request.form['venue_id'] + ';')
+    rows = connect('SELECT MeterName, MeterType FROM meters WHERE MeterType = ' + request.form['venue_id'] + ';')
     return render_template('my-result.html', rows=rows)
 
 # handle query POST and serve result web page
